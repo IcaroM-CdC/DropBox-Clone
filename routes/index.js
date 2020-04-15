1108ab660b6ed3baa1430544ca614c6988ffbd24
IcaroM-CdC <icaro.moreira@protonmail.com>
2020-04-15 19:40:08 -0300

    Implementaçao da rota para abrir arquivos no browser

--- routes/index.js
+++ routes/index.js
@@ -1,28 +1,87 @@
 const Express = require('express');
 const Formidable = require("formidable")
 const Router = Express.Router();
+const FileSystem = require("fs")
 
 /* GET home page. */
 Router.get('/', function(Requisicao, Resposta, Next) {
 
-  Resposta.render('index', { title: 'Express' });
+    Resposta.render('index', { title: 'Express' });               
 
 })
 
-Router.post("/upload", function(Requisicao, Resposta, Next){
+Router.post("/upload", function(Requisicao, Resposta, Next) {
 
-  var Form = new Formidable.IncomingForm({
-    uploadDir: "./upload",
-    keepExtensions: true
-  })
-
-  Form.parse(Requisicao, function(Erro, Fields, Arquivos) {
-
-    Resposta.json({
-      Arquivos: Arquivos
+    var Form = new Formidable.IncomingForm({
+        
+        uploadDir: "./upload",
+        keepExtensions: true
+    
     })
 
-  })
+    Form.parse(Requisicao, function(Erro, Fields, Arquivos) {
+
+        Resposta.json({
+            Arquivos: Arquivos
+        })
+
+    })
+})
+
+Router.get("/arquivos", function(Requisicao, Resposta, Next) {
+
+    var Path = "./" + Requisicao.query.path
+
+    if (FileSystem.existsSync(Path)) {
+
+        FileSystem.readFile(Path, (Erro, Arquivo) => {
+
+            if (Erro) {
+
+                console.error
+                Resposta.status(400).json({
+                    
+                    error: Erro
+                
+                })
+            } 
+
+            else {
+                Resposta.status(200).end(Arquivo)
+            }
+        })
+
+    } 
+
+    else { Resposta.status(404).json({  Erro: "Arquivo não encontrado"  })}
+
+})
+
+Router.delete("/arquivos", function(Requisicao, Resposta, Next) {
+    
+    var Form = new Formidable.IncomingForm({
+        uploadDir: "./upload",
+        keepExtensions: true
+    })
+
+    Form.parse(Requisicao, function(Erro, Fields, Arquivos) {
+
+        var Path = "./" + Fields.path
+
+        if (FileSystem.existsSync(Path)) {
+
+            FileSystem.unlink(Path, Erro => {
+            
+                if (Erro) {  Resposta.status(400).json({Erro})  }
+        
+            })
+        } 
+    
+        else {
+
+            Resposta.json({  Fields: Fields  })
+        }
+    })
 })
 
 module.exports = Router;
